ed2b8330813de252c39bd39ae1b53451f94d6c5a
Bhadan <bhadanpaul@gmail.com>
2026-05-14 14:29:20 +0600

    Myapp

--- lib/app.dart
+++ lib/app.dart
@@ -3,7 +3,6 @@ import 'package:to_do_app/Home_page.dart';
 
 class Myapp extends StatelessWidget {
   const Myapp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
